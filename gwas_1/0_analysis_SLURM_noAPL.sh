e7b5d762b32e14fd3a291f8f21d8b98a819623f3
diyanathsr <diyanath91@gmail.com>
2026-07-06 15:36:51 +0000

    removed email addresses from sh/slurm

--- gwas_1/0_analysis_SLURM_noAPL.sh
+++ gwas_1/0_analysis_SLURM_noAPL.sh
@@ -34,7 +34,6 @@ do
 #SBATCH --exclude=ln03,sb017
 #SBATCH --export=ALL
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=Wei-Yu.Lin@newcastle.ac.uk
 #####SBATCH --array=1-22
 #SBATCH --chdir=${rootpath}/association
 
--- strip_email.sh
+++ strip_email.sh
@@ -2,8 +2,7 @@
 
 TARGET="Wei-Yu.Lin@newcastle.ac.uk"
 
-
-find . -type f -name "*.sh" | while read -r file; do
+find . -type f -name "*.slurm" | while read -r file; do
     echo "Processing: $file"
     sed -i "/$TARGET/d" "$file"
 done
